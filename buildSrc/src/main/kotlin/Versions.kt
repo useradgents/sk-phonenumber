b90403334082a68359c48dda9a6f35139671d152
sgueniot <s.gueniot@useradgents.com>
2023-04-07 07:34:56 +0200

    [FEAT] publish to jitpack

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val group = "com.github.useradgents.sk-phone"
     const val library = "0.0.1"
     const val frameworkGroup = "com.github.useradgents.skot"
-    const val framework = "1.2.7-ua"
+    const val framework = "1.2.10-ua"
     const val libphonenumber = "8.13.5"
 }
